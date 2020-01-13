8253665bacf03571391c93aa793accc6da777e02
Ola <elhazne@gmail.com>
2020-01-13 14:40:56 +0100

    header component tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Header } from './components/header/Header'
+
+import "materialize-css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       
     </div>
   );
---
+++ src/components/header/header.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Header } from './Header'
+
+import { findByTestAttr } from '../../../utils/utils'
+
+const setUp = ( props = {} ) => {
+    const component = shallow(<Header {...props}/>)
+    return component
+}
+
+
+describe('Header', () => {
+
+    let component
+
+    beforeEach( () => {
+        component = setUp()
+    }) 
+
+    it('should be rendered', () => {   
+        const wrapper = findByTestAttr(component, 'nav')
+        expect(wrapper.length).toBe(1)
+    })
+
+    it('should render a logo', () => {        
+        const wrapper = findByTestAttr(component, 'logo')
+        expect(wrapper.length).toBe(1)
+    })
+})
